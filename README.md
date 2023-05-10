669649f5f2401e8e29458a1a786bd598abf4fcef
DoyleyLAD <38313214+DoyleyLAD@users.noreply.github.com>
2023-05-10 20:55:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# localStack-helper
+
+## Descriptiom
+Run Localstack using docker and helps script to create and delete AWS Resources.
+
+## Prerequisites
+Install Docker Desktop
+Install AWS CLI
+
+## Run Docker with LocalStack
+
+Option 1 :
+
+Run the following command in the directory that contains the docker-compose.yaml file. The command will run LocalStack in the background based of this projects docker-compose.yaml file. 
+
+Run Command : 
+```bash
+docker-compose up -d
+```
+
+Option 2 ; 
+
+Run the following command to run LocalStack via `docker run`
+
+Run Command : 
+```bash
+docker run --rm -it -p 4566:4566 -p 4571:4571 localstack/localstack
+```
+
+## Test LocalStack
+
+Command will return nothing, you should see no errors.
+
+Run Command :
+ ```bash
+ AWS_ACCESS_KEY_ID=localstack AWS_SECRET_ACCESS_KEY=locslstack aws --endpoint-url=http://localhost:4566 s3 ls
+ ```
+
+## Use resource-manager.sh
---
+++ resource-manager.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+RESOURCE=$1
+RESOURCE_NAME=$2
+VALUE=$3
+REGION="eu-west-2"
+AWS=$() 
+
+aws_ls() {
+    AWS_ACCESS_KEY_ID=localstack \
+    AWS_SECRET_ACCESS_KEY=locslstack \
+    aws --endpoint-url=http://localhost:4566 "$@"
+}
+
+if [ $RESOURCE = "S3" ] || [ $RESOURCE = "s3" ]; then
+    aws_ls s3api create-bucket \
+        --bucket $RESOURCE_NAME \
+        --region $REGION \
+        --create-bucket-configuration LocationConstraint=$REGION
+    aws_ls s3 ls
+fi
+
+if [ $RESOURCE = "SQS" ] || [ $RESOURCE = "sqs" ]; then
+    aws_ls sqs create-queue \
+        --queue-name $RESOURCE_NAME \
+        --region $REGION
+    aws_ls sqs list-queues --region $REGION 
+fi
+
+if [ $RESOURCE = "param" ] || [ $RESOURCE = "PS" ] || [ $RESOURCE = "ps" ]; then
+    aws_ls ssm put-parameter \
+        --name $RESOURCE_NAME \
+        --value $VALUE \
+        --type "SecureString" \
+        --region $REGION 
+    aws_ls ssm describe-parameters --region $REGION 
+fi
+
+if [ $RESOURCE = "list" ]; then
+    echo ""
+    echo "------------------------------------------------------------------" 
+    echo "S3 Buckets:" 
+    echo "------------------------------------------------------------------" 
+    aws_ls s3 ls
+    echo "------------------------------------------------------------------" 
+    echo "SQS:" 
+    echo "------------------------------------------------------------------" 
+    aws_ls sqs list-queues --region $REGION
+    echo "------------------------------------------------------------------" 
+    echo "Parameter Store:" 
+    echo "------------------------------------------------------------------" 
+    aws_ls ssm describe-parameters --region $REGION | grep "Name"
+    echo "------------------------------------------------------------------" 
+fi
